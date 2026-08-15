0015704bb79e4c3a6f564b17325bc3f04b21b729
thesyntax11 <269287763+thesyntax11@users.noreply.github.com>
2026-08-15 21:38:52 +0000

    Explicitly qualify Timer as System.Windows.Forms.Timer in engine and form files
    Co-authored-by: arena-agent <297053741+arena-agent@users.noreply.github.com>

--- JumpscareEngine.cs
+++ JumpscareEngine.cs
@@ -6,6 +6,7 @@ using System.Media;
 using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Timer = System.Windows.Forms.Timer;
 
 namespace ChaosVisualAudioSimulation
 {
--- PopupEngine.cs
+++ PopupEngine.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Threading;
 using System.Windows.Forms;
+using Timer = System.Windows.Forms.Timer;
 
 namespace ChaosVisualAudioSimulation
 {
